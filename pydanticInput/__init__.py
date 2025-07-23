9d4e2edf8d6d780341b1e6ff7efb76086b3f46c3
M Laraib Ali <laraibg786@outlook.com>
2025-07-23 23:24:52 +0500

    feat: enable dynamic versioning via importlib.metadata and git tags using hatch-vcs - add `hatch-vcs` as build dependency.

--- pydanticInput/__init__.py
+++ pydanticInput/__init__.py
@@ -1,2 +1,10 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from pydanticInput.dispatch import type_dispatch
 from pydanticInput.main import Input
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    __version__ = "0.0.0"
